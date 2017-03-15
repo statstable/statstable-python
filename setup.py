15a74f8305712153cbd69d970b1c9bb3e8abba93
Ben Gunton <ben@parkertrading.ca>
2017-03-15 12:36:54 -0700

    updated release to 0.2

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
   name = 'statstable',
   packages = ['statstable'],
-  version = '0.1',
+  version = '0.2',
   description = 'A wrapper to the StatStable API to retreive sports statistics',
   author = 'StatStable',
   author_email = 'info@statstable.com',
   url = 'https://github.com/statstable/statstable-python', 
-  download_url = 'https://github.com/statstable/statstable-python/archive/0.1.tar.gz', 
+  download_url = 'https://github.com/statstable/statstable-python/archive/0.2.tar.gz', 
   keywords = ['statstable', 'api', 'wrapper', 'sports', 'statistics', 'nba', 'mlb'],
   install_requires = [
     'requests',
