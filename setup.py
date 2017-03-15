b41a6a6dd24f6be081f0a800661fba42f76be374
Ben Gunton <ben@parkertrading.ca>
2017-03-15 09:33:32 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'statstable',
+  packages = ['statstable'],
+  version = '0.1',
+  description = 'A wrapper to the StatStable API to retreive sports statistics',
+  author = 'StatStable',
+  author_email = 'info@statstable.com',
+  url = 'https://github.com/statstable/statstable-python', 
+  download_url = 'https://github.com/statstable/statstable-python/archive/0.1.tar.gz', 
+  keywords = ['statstable', 'api', 'wrapper', 'sports', 'statistics', 'nba', 'mlb'],
+  classifiers = [],
+)
